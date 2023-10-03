c5dd435fa191b49dbb818a2a25a0be67e085b7c2
Anfitrion <tendondevastador@hotmail.com>
2023-10-03 14:51:29 -0500

    [+] Prevenimos que el bot muera cuando no haya backend

--- src/eventos/registrarUsuarios.ts
+++ src/eventos/registrarUsuarios.ts
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const API_URL = process.env.API_URL;
-const POST_URL = `${API_URL}/usuarios`;
 
 export default async function registrarUsuarios(message: any) {
   if (message.author.bot) return;
@@ -16,8 +15,26 @@ export default async function registrarUsuarios(message: any) {
     avatar: await message.author.avatarURL({ size: 512 }),
   };
 
+  const usuarioExistente = await axios
+    .get(`${API_URL}/usuarios/${infoUsuario.discord_id}`)
+    .then((response) => response.data)
+    .catch((error) => console.error(error));
+
+  if (!usuarioExistente) {
+    console.log("Backend no disponible");
+    return;
+  }
+
+  if (usuarioExistente) {
+    await axios
+      .put(`${API_URL}/usuarios/${infoUsuario.discord_id}`, infoUsuario)
+      .then((response) => console.log(response.data))
+      .catch((error) => console.error(error));
+    return;
+  }
+
   await axios
-    .post(POST_URL, infoUsuario)
+    .post(`${API_URL}/usuarios`, infoUsuario)
     .then((response) => console.log(response.data))
     .catch((error) => console.error(error));
   return;
